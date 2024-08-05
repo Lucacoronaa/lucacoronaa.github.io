5129ae5c088f447134c50eee1291079251a14748
Lucacoronaa <luca.corona0086@gmail.com>
2024-08-05 12:23:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function copyEmail() {
     // Seleziona l'input nascosto che contiene l'indirizzo email
     var emailInput = document.getElementById("email");
     
-    // Seleziona il testo nell'input
+    // Seleziona il testo nell'input 
     emailInput.select();
     emailInput.setSelectionRange(0, 99999); // Per dispositivi mobili
 
